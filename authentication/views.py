184ddcdb24ce72ece17177d09609b1697e439aa8
Vivek <chettribibek2020@gmail.com>
2023-06-19 14:29:19 +0545

    update image problem fix

--- authentication/views.py
+++ authentication/views.py
@@ -1,11 +1,11 @@
 
-from .models import Profile
 from django.shortcuts import redirect, render
-from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from authentication.models import CustomUser
 from django.contrib.auth import authenticate,login,logout
-
+from django.contrib.auth import update_session_auth_hash
+from authentication.models import Profile
 
 # Create your views here.
 def home(request):
@@ -31,6 +31,18 @@ def signin(request):
     if request.method == 'POST':
        email = request.POST.get('email')
        password = request.POST.get('password')
+
+       #validation for login
+
+    #    if user.object.filter(email!=email):
+    #        messages.error(request,"Email doesn't match! ")
+    #        return redirect("home")
+       
+    #    if user.object.filter(password!=password):
+    #        messages.error(request,"Password doesn't match!")
+    #        return redirect("home")
+       
+
        user = authenticate(request,email=email, password=password)
     #    print(user)
        if user is not None:
@@ -49,21 +61,53 @@ def signout(request):
     return redirect('home')
 
 
+# Profile Update
+@login_required
 def profile(request):
-    user=CustomUser.objects.get(id=request.user.id)
-    profile = Profile.objects.filter(user=user).values('image')
-    img =""
-    for pf in profile:
-        img = pf
-    context={'user':user,'profile':img}
-    return render(request,"profile.html",context)
+    user=request.user
+    profile=Profile.objects.get(user=user)
+    if request.method == 'POST':
+        if 'image' in request.FILES:
+            image=request.FILES['image']
+            profile.image=image
+        Username = request.POST['username']
+        Email = request.POST['email']
+        Phone = request.POST['phone']
+        user = request.user
 
+        user.username = Username
+        user.email = Email
+        #user.image = image
+        user.phone = Phone
+        user.save()
+        profile.save()
+        messages.success(request,"Profile Update Successfully!!!")
+        return redirect('profile')
+    return render(request, 'authentication/profile.html', {'user': request.user, 'profile':profile})
 
+# Change Password
+# @login_required
 def changePassword(request):
+    # if request.method == 'POST':
+    #     form = PasswordChangeForm(user=request.user, data=request.POST)
+    #     if form.is_valid():
+    #         user = form.save()
+    #         update_session_auth_hash(request, user)
+    #         messages.success(request, 'Your password was successfully changed.')
+    #         return redirect('changePassword')
+    # else:
+    #     form = PasswordChangeForm(user=request.user)
+    return render(request, 'authentication/changePassword.html')
 
-    return render(request,"authentication/changePassword.html")
-
-
-def resetPassword(request):
-
-    return render(request,"authentication/resetPassword.html")
+# Reset Password
+# def resetPassword(request):
+#     if request.method == 'POST':
+#         form = SetPasswordForm(user=request.user, data=request.POST)
+#         if form.is_valid():
+#             form.save()
+#             update_session_auth_hash(request, form.user)
+#             messages.success(request, 'Your password was successfully reset.')
+#             return redirect('resetPassword')
+#     else:
+#         form = SetPasswordForm(user=request.user)
+#     return render(request, 'authentication/resetPassword.html', {'form': form})
